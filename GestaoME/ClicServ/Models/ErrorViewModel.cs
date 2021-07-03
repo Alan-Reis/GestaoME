7c26593bee1bc2886e8cc83c342c844082aba222
Alan-Reis <69443030+Alan-Reis@users.noreply.github.com>
2021-07-02 23:53:43 -0300

    Init

---
+++ GestaoME/ClicServ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClicServ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
