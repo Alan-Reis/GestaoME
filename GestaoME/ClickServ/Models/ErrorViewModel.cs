d1e9acd4ad6de739bc3150d3141438fff1c343fc
Alan C. Reis <alanc.reis@hotmail.com>
2021-09-28 09:22:40 -0300

    Exclusão dos projetos anteriores e criação do mvc

---
+++ GestaoME/ClickServ/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ClickServ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
