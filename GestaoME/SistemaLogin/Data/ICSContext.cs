45686f338014f4a36162de79b2361c597a7bb5fa
Alan C. Reis <69443030+Alan-Reis@users.noreply.github.com>
2022-04-29 22:48:41 -0300

    Delete GestaoME/SistemaLogin directory

--- GestaoME/SistemaLogin/Data/ICSContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SistemaLogin.Data
-{
-    public class ICSContext
-    {
-    }
-}
--- GestaoME/SistemaLogin/Models/Login.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SistemaLogin.Models
-{
-    public class Login
-    {
-        public string Nome { get; set; }
-        public string Usuario { get; set; }
-        public string  Senha { get; set; }
-    }
-}
